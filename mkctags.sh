fca0403aa0bc60d959a71afcee285ec5ea49effa
Journeyer J. Joh <oosaprogrammer@gmail.com>
2012-08-13 19:25:18 +0900

    bug fix for langmap

--- mkctags.sh
+++ mkctags.sh
@@ -2,5 +2,6 @@
 
 rm -rf tags
 
-ctags --langmap=C++:.inc --c++-kinds=+p --fields=+iaS --extra=+fq --sort=foldcase -R .
+ctags --langmap=C++:+.inc --c++-kinds=+p --fields=+iaS --extra=+fq --sort=foldcase -R .
+
 
