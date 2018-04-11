88d55ad1f1c604a6fcb652372ffccf6d3bf52f4b
Journeyer Joh <oosaprogrammer@gmail.com>
2018-04-11 15:00:01 +0900

    install.sh fixed

--- install.sh
+++ install.sh
@@ -4,11 +4,8 @@
 if [ -e ".vimrc" ]; then
 if [ -e ".cncpp.vimrc" ]; then
 if [ -e ".system.vimrc" ]; then
-if [ -e ".php.vimrc" ]; then
 if [ -e "mkctags.sh" ]; then
-if [ -e "mkptags.sh" ]; then
 if [ -e "mkcscope.sh" ]; then
-if [ -e "mkpscope.sh" ]; then
 if [ -e "rmtags.sh" ]; then
 
 # Remove previously installed symbolic links
@@ -20,9 +17,6 @@ if [ -L "$HOME/.cncpp.vimrc" ]; then
 fi
 if [ -L "$HOME/.system.vimrc" ]; then
   rm -f $HOME/.system.vimrc
-fi
-if [ -L "$HOME/.php.vimrc" ]; then
-  rm -f $HOME/.php.vimrc
 fi
 
 # Backup your .vim/ and .vimrc in your home directory if they exist
@@ -45,13 +39,12 @@ fi
 ln -s -T `pwd`/.vimrc $HOME/.vimrc
 ln -s -T `pwd`/.cncpp.vimrc $HOME/.cncpp.vimrc
 ln -s -T `pwd`/.system.vimrc $HOME/.system.vimrc
-ln -s -T `pwd`/.php.vimrc $HOME/.php.vimrc
 
 # Install Vundle
-if [ -e "$HOME/.vim/bundle/vundle" ]; then
-  rm -rf $HOME/.vim/bundle/vundle
+if [ -e "$HOME/.vim/bundle/Vundle.vim" ]; then
+  rm -rf $HOME/.vim/bundle/Vundle.vim
 fi
-git clone https://github.com/gmarik/vundle.git $HOME/.vim/bundle/vundle
+git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
 
 mkdir -p $HOME/.vim/syntax
 ln -s -T `pwd`/php.vim $HOME/.vim/syntax/php.vim
@@ -59,7 +52,7 @@ ln -s -T `pwd`/php.vim $HOME/.vim/syntax/php.vim
 # Open vim and :BundleInstall
 echo
 echo ==============================================
-echo You must Open vim and :BundleInstall yourself!
+echo You must Open vim and :PluginInstall yourself!
 echo ==============================================
 echo
 
@@ -67,15 +60,9 @@ echo
 if [ -L "/usr/local/bin/mkctags.sh" ]; then
   sudo rm -f /usr/local/bin/mkctags.sh
 fi
-if [ -L "/usr/local/bin/mkptags.sh" ]; then
-  sudo rm -f /usr/local/bin/mkptags.sh
-fi
 
 if [ -L "/usr/local/bin/mkcscope.sh" ]; then
   sudo rm -f /usr/local/bin/mkcscope.sh
-fi
-if [ -L "/usr/local/bin/mkpscope.sh" ]; then
-  sudo rm -f /usr/local/bin/mkpscope.sh
 fi
 
 if [ -L "/usr/local/bin/rmtags.sh" ]; then
@@ -90,23 +77,16 @@ fi
 
 # Create new symbolic links to mkcscope.sh and mkctags.sh and rmtags.sh
 sudo ln -s -T `pwd`/mkctags.sh /usr/local/bin/mkctags.sh
-sudo ln -s -T `pwd`/mkptags.sh /usr/local/bin/mkptags.sh
 sudo ln -s -T `pwd`/mkcscope.sh /usr/local/bin/mkcscope.sh
-sudo ln -s -T `pwd`/mkpscope.sh /usr/local/bin/mkpscope.sh
 sudo ln -s -T `pwd`/rmtags.sh /usr/local/bin/rmtags.sh
 sudo ln -s -T `pwd`/svndiff-meld.sh /usr/local/bin/svndiff-meld.sh
 sudo ln -s -T `pwd`/svndiff-vim.sh /usr/local/bin/svndiff-vim.sh
 sudo chown -h `whoami`:`whoami` /usr/local/bin/mkctags.sh
-sudo chown -h `whoami`:`whoami` /usr/local/bin/mkptags.sh
 sudo chown -h `whoami`:`whoami` /usr/local/bin/mkcscope.sh
-sudo chown -h `whoami`:`whoami` /usr/local/bin/mkpscope.sh
 sudo chown -h `whoami`:`whoami` /usr/local/bin/rmtags.sh
 sudo chown -h `whoami`:`whoami` /usr/local/bin/svndiff-meld.sh
 sudo chown -h `whoami`:`whoami` /usr/local/bin/svndiff-vim.sh
 
-fi
-fi
-fi
 fi
 fi
 fi
