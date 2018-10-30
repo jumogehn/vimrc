6dd73e6386718924fa1c9b9b230661116651f6ba
Journeyer Joh <oosaprogrammer@gmail.com>
2018-10-30 15:26:54 +0900

    added semi-colon

--- mkcscope.bat
+++ mkcscope.bat
@@ -1,7 +1,7 @@
 
 del cscope.files cscope.out
 
-where /r %CD% *.c *.cpp *.cc *.h *.hpp *.java *.s *.S *.inc *.def > cscope.files
+where /r "%CD%" *.c *.cpp *.cc *.h *.hpp *.java *.s *.S *.inc *.def > cscope.files
 
 cscope -i cscope.files
 
